787a8d24b1e6b838d32be7c465ce946df8fb0f6f
mareks09 <marekspl09@gmail.com>
2026-04-21 09:48:25 +0300

    Add files via upload

---
+++ Pd.py
@@ -0,0 +1,28 @@
+import json
+
+with open("uzd3.json","r",encoding="utf-8") as datne:
+    dati = json.load(datne)
+    print(dati)
+
+
+for i in dati:
+    uznemumi = []
+    vid_cena = []
+    print(uznemumi.get["uznemumi"])
+
+
+
+
+vardnica=[]
+print(vardnica.update["uanemumi"]["vid_cena"])
+
+
+with open("akciju_analize.csv","r",encoding="utf-8") as datne:
+
+
+
+
+
+
+
+
